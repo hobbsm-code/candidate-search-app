33a3b94d83508e97fac1dc775241b6d79f04de8f
MAH_GitUser <hobbsm121@gmail.com>
2024-11-21 13:51:56 -0500

    Created candidate interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+
+export default interface Candidate {
+    readonly name: string | '';
+    readonly login: string | ''; 
+    readonly location: string | '';
+    readonly avatar_url: string | '';
+    readonly email: string | '';
+    readonly html_url: string | '';
+    readonly company: string | '';
+}
